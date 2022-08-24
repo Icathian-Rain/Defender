bad953a71f5983ed894ae087309d15921fb5d990
IcathianRain <2205794866@qq.com>
2022-08-24 10:47:44 +0800

    add syringe 注射器

---
+++ src/syringe/syringe.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<windows.h>
+#include<detours.h>
+#include<thread>
+#pragma comment(lib, "detours.lib")
+using namespace std;
+int main(int argc, char* argv[]) {
+	// wchar_t fileName[256] = L"";
+	// MultiByteToWideChar(CP_ACP, 0, argv[0], strlen(argv[1]), fileName, sizeof(fileName));
+	// wprintf(L"%s\n", fileName);
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+	si.cb = sizeof(STARTUPINFO);
+	// 文件夹路径
+	WCHAR DirPath[MAX_PATH + 1];
+	wcscpy_s(DirPath, MAX_PATH, L"C:\\Users\\22057\\Desktop\\softwareSecurity\\src\\DLLMain\\x64\\Debug");	// dll文件夹
+	char DLLPath[MAX_PATH + 1] = "C:\\Users\\22057\\Desktop\\softwareSecurity\\src\\DLLMain\\x64\\Debug\\DllMain.dll"; // dll的地址
+	WCHAR EXE[MAX_PATH + 1] = { 0 };
+	// wcscpy_s(EXE, MAX_PATH, fileName);
+	// wcscpy_s(EXE, MAX_PATH, L"C:\\Users\\22057\\Desktop\\softwareSecurity\\src\\testEXE\\X64\\Debug\\testEXE.exe");
+	wcscpy_s(EXE, MAX_PATH, L"C:\\Users\\22057\\Desktop\\softwareSecurity\\MFCApplication2_64.exe");
+	if (DetourCreateProcessWithDllEx(EXE, NULL, NULL, NULL, TRUE,
+		CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED, NULL, DirPath,
+		&si, &pi, DLLPath, NULL)) {
+		// 恢复线程
+		ResumeThread(pi.hThread);
+
+		WaitForSingleObject(pi.hProcess, INFINITE);
+	}
+	else {
+		char error[100];
+		DWORD err = GetLastError();
+		sprintf_s(error, sizeof(err)+1, "%d", err);
+	}
+	return 0;
+}
