8cfd39d9780f2831b770168549c2e290ee81d939
IcathianRain <2205794866@qq.com>
2022-09-07 17:18:08 +0800

    添加消息框

---
+++ src/defender/frontend/src/components/MsgAlert.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-alert
+    :closable = "false"
+    :title="titleList[type] + ' ' + msg['funcName']"
+    :type="typeList[type]"
+    :description="description"
+    show-icon
+    class="mb-5"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { defineProps } from "vue";
+
+const typeList = ["success", "warning", "error"];
+const titleList = ["低风险操作", "中风险操作", "高风险操作"];
+let type = ref(0);
+let description = ref("");
+
+const props = defineProps({
+    msg: {
+        type: Object,
+        required: true,
+    }
+});
+
+description.value = props.msg["info"]["time"]
+
+
+
+
+
+
+
+
+</script>
