f474306f109cf6ef74c1f7db60924f7dd0c5fdff
IcathianRain <2205794866@qq.com>
2022-09-19 08:54:21 +0800

    添加工具库utils

--- src/DllMain/msg.h
+++ src/DllMain/msg.h
@@ -2,78 +2,10 @@
 
 #include <string>
 #include <time.h>
+#include "utils.h"
 #include "base64.h"
 
-std::string GbkToUtf8(const char *src_str)
-{
-	int len = MultiByteToWideChar(CP_ACP, 0, src_str, -1, NULL, 0);
-	wchar_t* wstr = new wchar_t[len + 1];
-	memset(wstr, 0, len + 1);
-	MultiByteToWideChar(CP_ACP, 0, src_str, -1, wstr, len);
-	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
-	char* str = new char[len + 1];
-	memset(str, 0, len + 1);
-	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
-	std::string strTemp = str;
-	if (wstr) delete[] wstr;
-	if (str) delete[] str;
-	return strTemp;
-}
 
-std::string Utf8ToGbk(const char *src_str)
-{
-	int len = MultiByteToWideChar(CP_UTF8, 0, src_str, -1, NULL, 0);
-	wchar_t* wszGBK = new wchar_t[len + 1];
-	memset(wszGBK, 0, len * 2 + 2);
-	MultiByteToWideChar(CP_UTF8, 0, src_str, -1, wszGBK, len);
-	len = WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, NULL, 0, NULL, NULL);
-	char* szGBK = new char[len + 1];
-	memset(szGBK, 0, len + 1);
-	WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, szGBK, len, NULL, NULL);
-	std::string strTemp(szGBK);
-	if (wszGBK) delete[] wszGBK;
-	if (szGBK) delete[] szGBK;
-	return strTemp;
-}
-
-std::string wchar2string(const wchar_t* wchar)
-{
-    std::string str;
-    int len = WideCharToMultiByte(CP_UTF8, 0, wchar, -1, NULL, 0, NULL, NULL);
-    char* chRtn = new char[len + 1];
-    WideCharToMultiByte(CP_UTF8, 0, wchar, -1, chRtn, len, NULL, NULL);
-    str = chRtn;
-    delete[] chRtn;
-    return str;
-}
-
-
-std::string bytes2string(const BYTE * bytes, int len)
-{
-    std::string str = "";
-    for (int i = 0; i < len; i++)
-    {
-        int byte = bytes[i] >> 4 & 0xf;
-        if (byte >= 0 && byte <= 9)
-        {
-            str += byte + '0';
-        }
-        else if (byte >= 10 && byte <= 15)
-        {
-            str += byte + 'A' - 10;
-        }
-        byte = bytes[i] & 0xf;
-        if (byte >= 0 && byte <= 9)
-        {
-            str += byte + '0';
-        }
-        else if (byte >= 10 && byte <= 15)
-        {
-            str += byte + 'A' - 10;
-        }
-    }
-    return str;
-}
 class Msg
 {
 public:
@@ -134,9 +66,9 @@ void Msg::setItem(const std::string& key, const std::string& value)
     {
         flag = 1;
     }
-    // temp_key = my_replace(temp_key, "\\", "\\\\");
+    temp_key = my_replace(temp_key, "\\", "\\\\");
     temp_key = base64_encode(temp_key);
-    // temp_value = my_replace(temp_value, "\\", "\\\\");
+    temp_value = my_replace(temp_value, "\\", "\\\\");
     temp_value = base64_encode(temp_value);
 
     m_msg += "\n\"" + temp_key + "\": \"" + temp_value + "\"";
