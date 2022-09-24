7a08d69fdbf0382ad8451512d42f6490dcbc6480
IcathianRain <2205794866@qq.com>
2022-09-24 10:03:34 +0800

    hookHeap格式化

--- src/DllMain/hookHeap.h
+++ src/DllMain/hookHeap.h
@@ -24,13 +24,21 @@ extern "C" __declspec(dllexport) HANDLE WINAPI NewHeapCreate(
 {
     // heap操作上锁
     setLock();
+    // 函数操作
     HANDLE ret = OldHeapCreate(fIOoptions, dwInitialSize, dwMaximumSize);
+    // 类型转换
+    std::string fIOoptions_tmp = dword2hex(fIOoptions);
+    std::string dwInitialSize_tmp = std::to_string(dwInitialSize);
+    std::string dwMaximumSize_tmp = std::to_string(dwMaximumSize);
+    std::string ret_tmp = dword2hex((DWORD)ret);
+    // 插入句柄
     handle_set.insert(ret);
+    // 构造消息
     Msg msg("HeapCreate");
-    msg.setItem("fIOoptions", dword2hex(fIOoptions));
-    msg.setItem("dwInitialSize", std::to_string(dwInitialSize));
-    msg.setItem("dwMaximumSize", std::to_string(dwMaximumSize));
-    msg.setItem("ret", dword2hex((DWORD)ret));
+    msg.setItem("fIOoptions", fIOoptions_tmp);
+    msg.setItem("dwInitialSize", dwInitialSize_tmp);
+    msg.setItem("dwMaximumSize", dwMaximumSize_tmp);
+    msg.setItem("ret", ret_tmp);
     client.send(msg.getMsg().c_str());
     // heap操作解锁
     releaseLock();
@@ -45,10 +53,14 @@ extern "C" __declspec(dllexport) BOOL WINAPI NewHeapDestory(
 {
     // heap操作上锁
     setLock();
+    // 函数操作
     BOOL ret = OldHeapDestroy(hHeap);
+    // 类型转换
+    std::string hHeap_tmp = dword2hex((DWORD)hHeap);
+    std::string ret_tmp = std::to_string(ret);
     Msg msg("HeapDestroy");
-    msg.setItem("hHeap", std::to_string((int)hHeap));
-    msg.setItem("ret", std::to_string(ret));
+    msg.setItem("hHeap", hHeap_tmp);
+    msg.setItem("ret", ret_tmp);
     client.send(msg.getMsg().c_str());
     // heap操作解锁
     releaseLock();
@@ -67,15 +79,22 @@ extern "C" __declspec(dllexport) LPVOID WINAPI NewHeapAlloc(
     SIZE_T dwBytes //	分配大小
 )
 {
+    // heap操作
     LPVOID ret = OldHeapAlloc(hHeap, dwFlags, dwBytes);
     if (isLocked() == false && handle_set.find(hHeap) != handle_set.end())
     {
+        // heap操作上锁
         setLock();
+        // 类型转换
+        std::string hHeap_tmp = dword2hex((DWORD)hHeap);
+        std::string dwFlags_tmp = dword2hex(dwFlags);
+        std::string dwBytes_tmp = std::to_string(dwBytes);
+        std::string ret_tmp = dword2hex((DWORD)ret);
         Msg msg("HeapAlloc");
-        msg.setItem("hHeap", dword2hex((DWORD)hHeap));
-        msg.setItem("dwFlags", dword2hex(dwFlags));
-        msg.setItem("dwBytes", std::to_string(dwBytes));
-        msg.setItem("ret", dword2hex((DWORD)ret));
+        msg.setItem("hHeap", hHeap_tmp);
+        msg.setItem("dwFlags", dwFlags_tmp);
+        msg.setItem("dwBytes", dwBytes_tmp);
+        msg.setItem("ret", ret_tmp);
         client.send(msg.getMsg().c_str());
         // heap操作解锁
         releaseLock();
@@ -94,15 +113,22 @@ extern "C" __declspec(dllexport) BOOL WINAPI NewHeapFree(
     _Frees_ptr_opt_ LPVOID lpMem // 指向要释放的内存块的指针
 )
 {
+    // heap操作
     BOOL ret = OldHeapFree(hHeap, dwFlags, lpMem);
     if (isLocked() == false && handle_set.find(hHeap) != handle_set.end())
     {
+        // heap操作上锁
         setLock();
         Msg msg("HeapFree");
-        msg.setItem("hHeap", dword2hex((DWORD)hHeap));
-        msg.setItem("dwFlags", std::to_string(dwFlags));
-        msg.setItem("lpMem", dword2hex((DWORD)lpMem));
-        msg.setItem("ret", std::to_string(ret));
+        // 类型转换
+        std::string hHeap_tmp = dword2hex((DWORD)hHeap);
+        std::string dwFlags_tmp = dword2hex(dwFlags);
+        std::string lpMem_tmp = dword2hex((DWORD)lpMem);
+        std::string ret_tmp = std::to_string(ret);
+        msg.setItem("hHeap", hHeap_tmp);
+        msg.setItem("dwFlags", dwFlags_tmp);
+        msg.setItem("lpMem", lpMem_tmp);
+        msg.setItem("ret", ret_tmp);
         client.send(msg.getMsg().c_str());
         releaseLock();
     }
